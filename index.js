7283ad468403b7df9a0138c77f1cba6fc174ff80
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-10 14:46:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function changeLeastFavoriteCustomer() {
 
 function attemptTwoFavoriteCustomers() {
   let favoriteCustomer = 'joe';
-  favoriteCustomer = 'jim';
+  let favoriteCustomer = 'jim';
 }
 
 
