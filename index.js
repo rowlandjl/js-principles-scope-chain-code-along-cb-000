e19bcab060bfc49ff1d3664cc900ab8bae1c667f
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-10 15:10:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-/* Your code goes here */
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  return bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'bob';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'bill';
+}
+
+let favoriteCustomer = 'jim';
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'jim bob';
+  
+  throw new SyntaxError('test broken', 'someFile.js', 10);
+  
+}
+
+
+
